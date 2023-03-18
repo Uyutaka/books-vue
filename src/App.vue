9b07d081ae11491a220da87fa1b89926ac4b3834
uyutaka <yutaka.urakami@colorado.edu>
2023-03-18 05:22:31 +0000

    S8 -> 107.Listening for emitted events in order to simplify user notifications

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header />
   <div>
-    <router-view />
+    <router-view @success="success" @error="error" @warning="warning" />
   </div>
   <Footer />
 </template>
@@ -10,6 +10,7 @@
 import Header from "./components/Header.vue"
 import Footer from "./components/Footer.vue"
 import { store } from "./components/store.js"
+import { notie } from "notie"
 
 const getCookie = (name) => {
   return document.cookie.split("; ").reduce((r, v) => {
@@ -44,25 +45,26 @@ export default {
       }
     }
   },
-  mounted() {
-    const payload = {
-      foo: "bar",
-    }
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-    headers.append("Authorization", "Bearer " + store.token);
-
-    const requestOptions = {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: headers,
-    }
-    fetch(process.env.VUE_APP_API_URL + "/admin/foo", requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
+  methods: {
+    success(msg) {
+      notie.alert({
+        type: 'success',
+        text: msg,
       })
-  },
+    },
+    error(msg) {
+      notie.alert({
+        type: 'error',
+        text: msg,
+      })
+    },
+    warning(msg) {
+      notie.alert({
+        type: 'warning',
+        text: msg,
+      })
+    }
+  }
 }
 </script>
 
