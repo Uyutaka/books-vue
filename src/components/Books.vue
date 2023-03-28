fb526a80f5ef476f6a9418134a941df6465a3548
uyutaka <yutaka.urakami@colorado.edu>
2023-03-28 02:38:40 +0000

    S9 -> 122.Adding genres to the books and filtering by genre

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -4,7 +4,25 @@
             <div class="col">
                 <h1 class="mt-3">Books</h1>
             </div>
+
             <hr>
+            <div class="filters text-center">
+                <span class="filter me-1" v-bind:class="{ active: currentFilter === 0 }"
+                    v-on:click="setFilter(0)">ALL</span>
+                <span class="filter me-1" v-bind:class="{ active: currentFilter === 7 }"
+                    v-on:click="setFilter(7)">CLASSIC</span>
+                <span class="filter me-1" v-bind:class="{ active: currentFilter === 2 }"
+                    v-on:click="setFilter(2)">FANTASY</span>
+                <span class="filter me-1" v-bind:class="{ active: currentFilter === 6 }"
+                    v-on:click="setFilter(6)">HORROR</span>
+                <span class="filter me-1" v-bind:class="{ active: currentFilter === 4 }"
+                    v-on:click="setFilter(4)">THRILLER</span>
+                <span class="filter me-1" v-bind:class="{ active: currentFilter === 1 }" v-on:click="setFilter(1)">SCIENCE
+                    FICTION</span>
+            </div>
+
+            <hr>
+
             <div>
                 <div class="card-group">
 
@@ -19,7 +37,11 @@
 
                                 <div class="card-body text-center">
                                     <h6 class="card-title">{{ b.title }}</h6>
-                                    {{ b.author.author_name }}
+                                    <span class="book-author">{{ b.author.author_name }}</span><br>
+                                    <small class="text-muted book-genre" v-for="(g, index) in b.genres" v-bind:key="g.id">
+                                        <em class="me-1">{{ g.genre_name }}<template
+                                                v-if="index !== (b.genres.length - 1)">,</template></em>
+                                    </small>
                                 </div>
                             </div>
                         </div>
@@ -52,9 +74,7 @@ export default {
                 if (this.error) {
                     this.$emit('error', response.message);
                 } else {
-
                     this.books = response.data.books;
-                    console.log(this.books);
                     this.ready = true;
                 }
             })
@@ -63,7 +83,37 @@ export default {
             })
     },
     methods: {
-
+        setFilter: function (filter) {
+            this.currentFilter = filter;
+        }
     },
 }
 </script>
+
+<style scoped>
+.filters {
+    height: 2.5em;
+}
+
+.filter {
+    padding: 6px 6px;
+    cursor: pointer;
+    border-radius: 6px;
+    transition: all 0.35s;
+    border: 1px solid silver;
+}
+
+.filter.active {
+    background: lightgreen;
+}
+
+.filter:hover {
+    background: lightgray;
+}
+
+
+.book-author,
+.book-genre {
+    font-size: 0.8em;
+}
+</style>
